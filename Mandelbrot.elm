3954e05541d890755b96d9baaad1e491b50b3c22
Adrian <adicirstei@gmail.com>
2016-11-03 17:24:00 +0200

    advanced a little

--- Mandelbrot.elm
+++ Mandelbrot.elm
@@ -1,7 +1,29 @@
 module Mandelbrot exposing (Model, init, view)
 
-type alias Model = {
-  width : Int
-  height : Int
-  computed : Dict Point Int
-}
+import Dict exposing (Dict)
+import Html exposing (..)
+
+
+type alias Point =
+    ( Int, Int )
+
+
+type alias Model =
+    { width : Int
+    , height : Int
+    , computed : Dict Point Int
+    }
+
+
+init : Int -> Model
+init size =
+    { width = size
+    , height = size
+    , computed = Dict.empty
+    }
+
+
+view : Model -> Html msg
+view model =
+    div []
+        (List.map viewRow [0..model.height])
